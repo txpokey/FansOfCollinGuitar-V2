7c6077bc1bcd57492860ebf90c03789ebcd0ce36
Bob Makowski <tritonpi-git0@yahoo.com>
2018-05-29 08:57:09 -0500

    F:-: added services module and linked it into the layout module. preparing stub for reading json from a file

--- fansOfCollinGuitarViaAngular/src/main/ts/src/app/layout/layout.module.ts
+++ fansOfCollinGuitarViaAngular/src/main/ts/src/app/layout/layout.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ServicesModule } from '../services/services.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ServicesModule
   ],
   declarations: [HeaderComponent, FooterComponent],
   exports: [HeaderComponent, FooterComponent]
