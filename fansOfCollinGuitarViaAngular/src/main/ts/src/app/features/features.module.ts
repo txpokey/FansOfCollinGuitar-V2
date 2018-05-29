8654cd8930ed4b38ac68be315c18d83e147d9cac
Bob Makowski <tritonpi-git0@yahoo.com>
2018-05-28 21:26:44 -0500

    T:-: added features module

---
+++ fansOfCollinGuitarViaAngular/src/main/ts/src/app/features/features.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FeaturesRoutingModule } from './features-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FeaturesRoutingModule
+  ],
+  declarations: []
+})
+export class FeaturesModule { }
