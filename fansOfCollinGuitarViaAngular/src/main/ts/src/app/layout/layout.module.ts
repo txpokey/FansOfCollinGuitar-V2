a2ca80d517a511613df488baa29832819135405b
Bob Makowski <tritonpi-git0@yahoo.com>
2018-06-02 11:29:17 -0500

    T:T: porting header : banner is up. carousel is up;  nav is up, but old routes

--- fansOfCollinGuitarViaAngular/src/main/ts/src/app/layout/layout.module.ts
+++ fansOfCollinGuitarViaAngular/src/main/ts/src/app/layout/layout.module.ts
@@ -4,12 +4,13 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ServicesModule } from '../services/services.module';
 import { CarouselComponent } from './carousel/carousel.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [HeaderComponent, FooterComponent, CarouselComponent],
-  exports: [HeaderComponent, FooterComponent, CarouselComponent]
+  declarations: [HeaderComponent, FooterComponent, CarouselComponent, NavbarComponent],
+  exports: [HeaderComponent, FooterComponent, CarouselComponent, NavbarComponent]
 })
 export class LayoutModule { }
