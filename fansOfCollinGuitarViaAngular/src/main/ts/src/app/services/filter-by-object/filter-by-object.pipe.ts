d481d6b14f2b7df143d1c5f27c51ee7bf49769ed
Bob Makowski <tritonpi-git0@yahoo.com>
2018-06-02 16:10:56 -0500

    F:-: porting news : checkpoint : tabs is bad, news is bad. just raw coding to get started

---
+++ fansOfCollinGuitarViaAngular/src/main/ts/src/app/services/filter-by-object/filter-by-object.pipe.ts
@@ -0,0 +1,28 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filterByObject'
+})
+export class FilterByObjectPipe implements PipeTransform {
+
+  transform(value: any, args?: any): any {
+    var ret : boolean = true ;
+    for( let key in args ) {
+      let candidate: boolean = false ;
+      if( value.hasOwnProperty(key) ) {
+        let inverter : boolean = false ;
+        let input = value[key] ;
+        let filterRaw = args[key] ;
+        let filter = filterRaw ;
+        if ( 0 === filterRaw.search("!") ) {
+          filter = filterRaw.slice(1) ;
+          inverter = true ;
+        }
+        candidate = filter == input ;
+        ret = inverter ? !candidate : candidate ;
+      }
+    }
+    return ret ;
+  }
+
+}
--- fansOfCollinGuitarViaAngular/src/main/ts/src/app/services/services.module.ts
+++ fansOfCollinGuitarViaAngular/src/main/ts/src/app/services/services.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FileAsSourceForJsonService} from "./file-as-source-for-json.service";
 import { TabStateComponent } from './tab-state/tab-state.component';
-
+import {FilterByObjectPipe} from "./filter-by-object/filter-by-object.pipe";
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [FileAsSourceForJsonService, TabStateComponent],
+  declarations: [FileAsSourceForJsonService, TabStateComponent, FilterByObjectPipe],
   exports: [TabStateComponent]
 })
 export class ServicesModule { }
