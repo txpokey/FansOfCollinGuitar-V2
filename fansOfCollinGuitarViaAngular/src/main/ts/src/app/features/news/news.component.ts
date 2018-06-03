9042f1a7c5bb10641d6a68960ae9fa216db9e157
Bob Makowski <tritonpi-git0@yahoo.com>
2018-06-02 20:34:38 -0500

    -:T: integrated include-template component into the news template. confirmed html being read from file system, but [innerHtml] running at wrong time :)

--- fansOfCollinGuitarViaAngular/src/main/ts/src/app/features/news/news.component.ts
+++ fansOfCollinGuitarViaAngular/src/main/ts/src/app/features/news/news.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import {TabStateComponent} from "../../services/tab-state/tab-state.component";
 import {IGuitarNewsConfig} from "./GuitarNewsConfig";
 import {FileAsSourceForJsonService} from "../../services/file-as-source-for-json.service";
+import {IncludeTemplateComponent} from "../../services/include-template/include-template.component";
 
 @Component({
   selector: 'guitar-news',
   templateUrl: './news.component.html',
-  providers: [FileAsSourceForJsonService,TabStateComponent],
+  providers: [FileAsSourceForJsonService,TabStateComponent, IncludeTemplateComponent],
   styleUrls: ['./news.component.css']
 })
 export class NewsComponent implements OnInit {
