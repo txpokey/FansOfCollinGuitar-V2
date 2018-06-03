de931d42d77d49a7d1d8e51ef02f7f76c31ffeef
Bob Makowski <tritonpi-git0@yahoo.com>
2018-06-03 18:14:55 -0500

    T:T: routing changed so landing page is 'guitarNews' route. 'welcome' is now stubbed out its component is not used.

--- fansOfCollinGuitarViaAngular/src/main/ts/src/app/app-routing.module.ts
+++ fansOfCollinGuitarViaAngular/src/main/ts/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { WelcomeComponent } from './welcome/welcome.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  { path: 'welcome', component: WelcomeComponent },
+  // { path: 'welcome', component: WelcomeComponent },
+  { path: 'welcome', redirectTo: 'guitarNews', pathMatch: 'full' },
+  { path: '', redirectTo: 'guitarNews', pathMatch: 'full' },
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
 ];
