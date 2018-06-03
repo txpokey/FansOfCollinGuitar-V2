c3033fbdf46b409674816525ec1bf4588e39c7f2
Bob Makowski <tritonpi-git0@yahoo.com>
2018-06-03 17:23:26 -0500

    F:-: killed old filterByObject code

--- fansOfCollinGuitarViaAngular/src/main/ts/src/app/pipes/0filter-by-object/filter-by-object.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterByObjectPipe } from './filter-by-object.pipe';
-
-describe('FilterByObjectPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterByObjectPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
