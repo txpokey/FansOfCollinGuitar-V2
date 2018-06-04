8255ddcf0cfb089ec48f882999f8da65c7dd0fe5
Bob Makowski <tritonpi-git0@yahoo.com>
2018-06-03 21:56:44 -0500

    T:T: feature: events : DONE : using data from array version of service for reading JSON from file

--- fansOfCollinGuitarViaAngular/src/main/ts/src/app/features/lists/events/events.component.ts
+++ fansOfCollinGuitarViaAngular/src/main/ts/src/app/features/lists/events/events.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FileAsSourceForJsonService} from "../../../services/file-as-source-for-json/file-as-source-for-json.service";
+import {IGuitarEvent} from "./GuitarEvent";
 
 @Component({
   selector: 'guitar-events',
@@ -8,10 +9,12 @@ import {FileAsSourceForJsonService} from "../../../services/file-as-source-for-j
 })
 export class EventsComponent implements OnInit {
 
+  guitarProgramEvents: IGuitarEvent[] ;
   constructor(private service: FileAsSourceForJsonService) {  }
 
   ngOnInit() {
     let myAny: any  = this.service.getEventsSetUp() ;
+    this.guitarProgramEvents = myAny ;
     console.log("eventsComponent is HERE:> " + myAny );
   }
 
