88c57273b79b17effd7f31b64094068092db4e91
Bob Makowski <tritonpi-git0@yahoo.com>
2018-06-10 18:36:25 -0500

    T:T: feature: class schedule : DEV : refactoring for clarity. added concept of report in post processing my httpClient output model

--- fansOfCollinGuitarViaAngular/src/main/ts/src/app/features/classroom/class-schedule/class-schedule-planner/class-schedule-planner.component.ts
+++ fansOfCollinGuitarViaAngular/src/main/ts/src/app/features/classroom/class-schedule/class-schedule-planner/class-schedule-planner.component.ts
@@ -12,141 +12,158 @@ import {isUndefined} from "util";
   styleUrls: ['./class-schedule-planner.component.css']
 })
 export class ClassSchedulePlannerComponent implements OnInit {
-  musicDeptCatalog : IMusicDeptCatalogByTerm[] ;
-  guitarProgramSchedule : IGuitarProgramCourseScheduleByTerm[] ;
-  musicCatalogMeta : any = {} ;
-  guitarSectionMeta : any = {} ;
-  constructor(private service: FileAsSourceForJsonService) {  }
+  musicDeptCatalog: IMusicDeptCatalogByTerm[];
+  guitarProgramSchedule: IGuitarProgramCourseScheduleByTerm[];
+  musicCatalogReportData: any = {};
+  guitarSectionReportData: any = {};
+
+  constructor(private service: FileAsSourceForJsonService) {
+  }
 
   ngOnInit() {
-    this.musicDeptCatalog = this.service.getMusicDeptCatalog() ;
-    this.guitarProgramSchedule = this.service.getGuitarProgramCourseSchedule() ;
-    this.computeGuitarSectionMaps();
-    this.computeMusicDisciplineMaps();
-    console.log("scheduleComponent is HERE!" );
+    this.musicDeptCatalog = this.service.getMusicDeptCatalog();
+    this.guitarProgramSchedule = this.service.getGuitarProgramCourseSchedule();
+    this.computeGuitarProgramReportData();
+    this.computeMusicCalalogReportData();
+    console.log("scheduleComponent is HERE!");
   }
 
-  // private composeSingleMap( a: any, b: any ): any {
-  //   let ret = { schoolSemester: a , schoolYear: b } ;
-  //   return ret ;
-  // }
-  private hashKey( key: any ): string {
-    return this.service.hashKey(key) ;
+  private hashKey(key: any): string {
+    return this.service.hashKey(key);
   }
 
-  findClassesFromMusicCatalogBySchoolTermAsArray( lookupKey: any ) : any {
-    let ret0 = this.musicCatalogMeta.groupBy.get( this.hashKey(lookupKey) ) ;
-    let ret = ret0.payload ;
-    return ret ;
-  }
-  findClassesFromMusicCatalogBySchoolTermAsArray_0( lookupKey: any ) : any {
-    let ret0 = this.musicCatalogMeta.groupBy.get( this.hashKey(lookupKey) ) ;
-    let ret1 = ret0.values() ;
-    let ret = Array.from( ret1 ) ;
-
-    return ret ;
+  findClassesFromMusicCatalogBySchoolTermAsArray(lookupKey: any): any {
+    let ret0 = this.musicCatalogReportData.groupBy.get(this.hashKey(lookupKey));
+    let ret = ret0.payload;
+    return ret;
   }
 
-  private computeGuitarSectionMaps(): void {
-    let collector: any[] = [] ;
-    let yearsFound: Set<number> = new Set( this.guitarProgramSchedule.map(obj => obj.schoolYear));
-    let semestersFound: Set<string> = new Set( this.guitarProgramSchedule.map(obj => obj.schoolSemester));
-    let guitarGroupBy = new Map() ;
-    let musicGroupBy = new Map() ;
-    this.guitarProgramSchedule.forEach( obj =>
-      obj.payload.forEach( pay => {
-        this.computeGroupByMapForGuitarSections( obj, pay, guitarGroupBy ) ;
-        this.computeGroupByMapForMusicCourses( obj, pay, musicGroupBy ) ;
-      }  )) ;
+  private computeGuitarProgramReportData(): void {
+    let yearsFound: Set<number> = new Set(this.guitarProgramSchedule.map(obj => obj.schoolYear));
+    let semestersFound: Set<string> = new Set(this.guitarProgramSchedule.map(obj => obj.schoolSemester));
+    let guitarGroupBy = new Map();
+    let musicGroupBy = new Map();
+    this.guitarProgramSchedule.forEach(obj =>
+      obj.payload.forEach(pay => {
+        this.computeGroupByMapForGuitarSections(obj, pay, guitarGroupBy);
+        this.computeGroupByMapForMusicCoursesUsingGuitarProgramData(obj, pay, musicGroupBy);
+      }));
 
-    this.guitarSectionMeta.yearsFound = yearsFound ;
-    this.guitarSectionMeta.semestersFound = semestersFound ;
-    this.guitarSectionMeta.groupBy = guitarGroupBy ;
-    this.musicCatalogMeta.groupBy = musicGroupBy ;
+    this.guitarSectionReportData.yearsFound = yearsFound;
+    this.guitarSectionReportData.semestersFound = semestersFound;
+    this.guitarSectionReportData.groupBy = guitarGroupBy;
+    this.guitarSectionReportData.musicCatalogGroupBy = musicGroupBy;
 
-    return ;
+    return;
   }
-  private computeGroupByMapForGuitarSections( obj: any , pay: any, g: Map<any,any> ): any {
-    let musicKey = { schoolSemester: obj.schoolSemester , schoolYear: obj.schoolYear, discipline: pay.Subj } ;
-    let guitarKey = { schoolSemester: obj.schoolSemester , schoolYear: obj.schoolYear, discipline: pay.Subj , course: pay.Crse } ;
-    let guitarKeyAsObject = { schoolSemester: obj.schoolSemester , schoolYear: obj.schoolYear, discipline: pay.Subj , course: pay.Crse } ;
-    let hash = this.hashKey( guitarKey );
-    let withGKey = guitarKey['gKeyAsObject'] = guitarKeyAsObject ;
-    let withMKey = guitarKey['mKeyAsObject'] = musicKey ;
-    let newPay = guitarKey['pay'] = pay ;
+
+  private computeGroupByMapForGuitarSections(obj: any, pay: any, g: Map<any, any>): any {
+    let musicKey = {schoolSemester: obj.schoolSemester, schoolYear: obj.schoolYear, discipline: pay.Subj};
+    let guitarKey = {
+      schoolSemester: obj.schoolSemester,
+      schoolYear: obj.schoolYear,
+      discipline: pay.Subj,
+      course: pay.Crse
+    };
+    let guitarKeyAsObject = {
+      schoolSemester: obj.schoolSemester,
+      schoolYear: obj.schoolYear,
+      discipline: pay.Subj,
+      course: pay.Crse
+    };
+    let hash = this.hashKey(guitarKey);
+    let withGKey = guitarKey['gKeyAsObject'] = guitarKeyAsObject;
+    let withMKey = guitarKey['mKeyAsObject'] = musicKey;
+    let newPay = guitarKey['pay'] = pay;
 
     let discovered = g.get(hash);
-    let depth = isUndefined(discovered) ? (discovered = []).push(guitarKey) : discovered.push( guitarKey ) ;
-    let revalue = g.set(hash,discovered);
-    let allKeysNow = Array.from( g.keys() );
-    let count = allKeysNow.length ;
-    return ;
+    let depth = isUndefined(discovered) ? (discovered = []).push(guitarKey) : discovered.push(guitarKey);
+    let revalue = g.set(hash, discovered);
+    let allKeysNow = Array.from(g.keys());
+    let count = allKeysNow.length;
+    return;
   }
-  private computeGroupByMapForMusicCourses( obj: any , pay: any, g: Map<any,any> ): any {
-    let musicKey = { schoolSemester: obj.schoolSemester , schoolYear: obj.schoolYear, discipline: pay.Subj } ;
-    let guitarKey = { schoolSemester: obj.schoolSemester , schoolYear: obj.schoolYear, discipline: pay.Subj , course: pay.Crse } ;
-    let guitarKeyAsObject = { schoolSemester: obj.schoolSemester , schoolYear: obj.schoolYear, discipline: pay.Subj , course: pay.Crse } ;
-    let musicKeyHash = this.hashKey( musicKey );
-    let guitarKeyHash = this.hashKey( guitarKey );
-    let withGKey = guitarKey['gKeyAsObject'] = guitarKeyAsObject ;
-    let withMKey = guitarKey['mKeyAsObject'] = musicKey ;
-    let newPay = guitarKey['pay'] = pay ;
+
+  private computeGroupByMapForMusicCoursesUsingGuitarProgramData(obj: any, pay: any, g: Map<any, any>): any {
+    let musicKey = {schoolSemester: obj.schoolSemester, schoolYear: obj.schoolYear, discipline: pay.Subj};
+    let guitarKey = {
+      schoolSemester: obj.schoolSemester,
+      schoolYear: obj.schoolYear,
+      discipline: pay.Subj,
+      course: pay.Crse
+    };
+    let guitarKeyAsObject = {
+      schoolSemester: obj.schoolSemester,
+      schoolYear: obj.schoolYear,
+      discipline: pay.Subj,
+      course: pay.Crse
+    };
+    let musicKeyHash = this.hashKey(musicKey);
+    let guitarKeyHash = this.hashKey(guitarKey);
+    let withGKey = guitarKey['gKeyAsObject'] = guitarKeyAsObject;
+    let withMKey = guitarKey['mKeyAsObject'] = musicKey;
+    let newPay = guitarKey['pay'] = pay;
 
     let discovered = g.get(musicKeyHash);
-    let depth = isUndefined(discovered) ? (discovered = new Map()).set( guitarKeyHash , guitarKey ) : discovered.set( guitarKeyHash , guitarKey ) ;
-    let revalue = g.set(musicKeyHash,discovered);
-    let allKeysNow = Array.from( g.keys() );
-    let count = allKeysNow.length ;
-    return ;
+    let depth = isUndefined(discovered) ? (discovered = new Map()).set(guitarKeyHash, guitarKey) : discovered.set(guitarKeyHash, guitarKey);
+    let revalue = g.set(musicKeyHash, discovered);
+    let allKeysNow = Array.from(g.keys());
+    let count = allKeysNow.length;
+    return;
   }
 
- private computeGroupByMapForMusicCourses0( schoolSemester: any , schoolYear: any, discipline: any , sux: any ): any {
-    let inputReference = this.guitarSectionMeta.groupBy ;
-    let hashForPick = this.hashKey( { schoolSemester: schoolSemester , schoolYear: schoolYear, discipline: discipline, sux: sux } );
-    let discoveredMappingByHashPicked = inputReference.get( hashForPick ) ;
-    let courseListAsArray = Array.of( discoveredMappingByHashPicked.map(
-        courseMapping => courseMapping['gKeyAsObject']
-    )) ;
-   return courseListAsArray ;
+  private computeMusicCalalogReportData(): void {
+    this.computerMusicCatalogReportSemesterByYearsData();
+    this.computerMusicCatalogReportGroupByData();
   }
 
-// TODO : breack this up and let the music catalog widget compute this stuff
-  private computeMusicDisciplineMaps(): void {
-    let collector: any[] = [] ;
-    let yearsFound: Set<number> = new Set( this.musicDeptCatalog.map(obj => obj.schoolYear));
-    let semestersFound: Set<string> = new Set( this.musicDeptCatalog.map(obj => obj.schoolSemester));
-    let crossProduct: Set<any> = new Set( this.musicDeptCatalog.map(obj => {
-      let groupByDiscipline = [] ;
-      let disciplines = new Set( obj.payload.map( data => data.discipline )) ;
-      let sorted = Array.from(disciplines).sort();
-      let item = { schoolSemester: obj.schoolSemester , schoolYear: obj.schoolYear, discipline: sorted } ;
-      return item ;
+  private computerMusicCatalogReportSemesterByYearsData(): void {
+    // let collector: any[] = [];
+    let yearsFound: Set<number> = new Set(this.musicDeptCatalog.map(obj => obj.schoolYear));
+    let semestersFound: Set<string> = new Set(this.musicDeptCatalog.map(obj => obj.schoolSemester));
+    let projectionOnDiscipline: Set<any> = new Set(this.musicDeptCatalog.map(obj => {
+      // let groupByDiscipline = [];
+      let disciplines = new Set(obj.payload.map(data => data.discipline));
+      let sortedDisciplines = Array.from(disciplines).sort();
+      let item = {schoolSemester: obj.schoolSemester, schoolYear: obj.schoolYear, discipline: sortedDisciplines};
+      return item;
     }));
-    this.musicCatalogMeta.yearsFound = yearsFound ;
-    this.musicCatalogMeta.semestersFound = semestersFound ;
-    this.musicCatalogMeta.semestersByYearsFound = Array.from(crossProduct) ;
-    // TODO : get better names...
-    let groupByCandidate = new Map() ;
-      this.musicDeptCatalog.forEach( term => {
-        term.payload.forEach( termPayloadItem => {
-          if( termPayloadItem.relevent ) {
-            this.computeMusicDepartmentCatalogGroupByItemFromTermPayloadItem( term, termPayloadItem, groupByCandidate ) ;
-          } ;
-        }) ;
-        console.log( "watchTheTerm:> " + term );
-      })
-    this.musicCatalogMeta.groupBy = groupByCandidate ;
-    return ;
+    this.musicCatalogReportData.yearsFound = yearsFound;
+    this.musicCatalogReportData.semestersFound = semestersFound;
+    this.musicCatalogReportData.semestersByYearsFound = Array.from(projectionOnDiscipline);
+    return
   }
-  private computeMusicDepartmentCatalogGroupByItemFromTermPayloadItem(term: any, termPayloadItem: any , groupByCandidate: Map<string,any>): void {
-    let hashInput = { schoolSemester: term.schoolSemester , schoolYear: term.schoolYear, discipline: termPayloadItem.discipline } ;
+
+  private computerMusicCatalogReportGroupByData(): void {
+    let groupByCandidate = new Map();
+    this.musicDeptCatalog.forEach(term => {
+      term.payload.forEach(termPayloadItem => {
+        if (termPayloadItem.relevent) {
+          this.computeMusicDepartmentCatalogGroupByItemFromTermPayloadItem(term, termPayloadItem, groupByCandidate);
+        }
+      });
+      console.log("watchTheTerm:> " + term);
+    })
+    this.musicCatalogReportData.groupBy = groupByCandidate;
+    return
+  }
+
+  private computeMusicDepartmentCatalogGroupByItemFromTermPayloadItem(term: any, termPayloadItem: any, groupByCandidate: Map<string, any>): void {
+    let hashInput = {
+      schoolSemester: term.schoolSemester,
+      schoolYear: term.schoolYear,
+      discipline: termPayloadItem.discipline
+    };
     let hashKey = this.hashKey(hashInput);
     let discovered = groupByCandidate.get(hashKey);
     // console.log( "watchTheHashKey:> " + hashKey );
 
-    let depth = isUndefined(discovered) ? (discovered = { hashInput: hashInput, payload: [termPayloadItem]}) : discovered.payload.push( termPayloadItem ) ;
+    let depth = isUndefined(discovered) ? (discovered = {
+      hashInput: hashInput,
+      payload: [termPayloadItem]
+    }) : discovered.payload.push(termPayloadItem);
     // console.log( "watchTheDiscovery:> " + discovered );
-    groupByCandidate.set(hashKey, discovered) ;
+    groupByCandidate.set(hashKey, discovered);
     return
   }
 
--- fansOfCollinGuitarViaAngular/src/main/ts/src/app/features/classroom/class-schedule/music-catalog-entry/music-catalog-entry.component.ts
+++ fansOfCollinGuitarViaAngular/src/main/ts/src/app/features/classroom/class-schedule/music-catalog-entry/music-catalog-entry.component.ts
@@ -7,36 +7,29 @@ import {ClassSchedulePlannerComponent} from "../class-schedule-planner/class-sch
   styleUrls: ['./music-catalog-entry.component.css']
 })
 export class MusicCatalogEntryComponent implements OnInit {
-
-  constructor(private planner: ClassSchedulePlannerComponent) {
-  }
-
-// { schoolSemester:  schoolTerm.schoolSemester , schoolYear: schoolTerm.schoolYear , discipline: musicDiscipline };
   @Input() semester: string;
   @Input() year: number;
   @Input() discipline: string;
 
-  isLookupInScope() {
-    let musicKey0 = {schoolSemester: 'spring', schoolYear: 2018, discipline: 'MUSI'};
+  constructor(private planner: ClassSchedulePlannerComponent) {
+  }
+
+  isLookupInScope() { // DEBUG
+    // let musicKey0 = {schoolSemester: 'spring', schoolYear: 2018, discipline: 'MUSI'};
     let musicKey = {schoolSemester: this.semester, schoolYear: this.year, discipline: this.discipline};
-    // let myLookup: any[] = this.planner.findClassesFromMusicCatalogBySchoolTermAsArray(musicKey);
     let myLookup: any[] = this.findClasses(musicKey);
-    console.log("musicKey:> " + musicKey);
-    console.log("myLookup:> " + myLookup.length);
+    // console.log("musicKey:> " + musicKey);
+    // console.log("myLookup:> " + myLookup.length);
   }
 
   findClasses( key: any ): any[] {
     let myLookup: any[] = this.planner.findClassesFromMusicCatalogBySchoolTermAsArray(key);
     return myLookup ;
   }
-  findClasses_0( key: any ): any[] {
-    let myLookup: any[] = this.planner.findClassesFromMusicCatalogBySchoolTermAsArray(key);
-    return myLookup ;
-  }
 
   ngOnInit() {
-    let myPlan = this.planner.musicCatalogMeta;
-    this.isLookupInScope();
+    // let myPlan = this.planner.musicCatalogReportData;
+    // this.isLookupInScope();
   }
 
 }
