7a8f311f7f64aac8c6847b5f4b1aa8d47bc37c30
Bob Makowski <tritonpi-git0@yahoo.com>
2018-06-11 09:16:28 -0500

    F:T: feature: class schedule : DEV : plumbing for ng-bootstrap modal is working. next: build out inputs to course-schedule-entry

--- fansOfCollinGuitarViaAngular/src/main/ts/src/app/features/classroom/class-schedule/course-schedule-entry/course-schedule-entry.component.ts
+++ fansOfCollinGuitarViaAngular/src/main/ts/src/app/features/classroom/class-schedule/course-schedule-entry/course-schedule-entry.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'course-schedule-entry',
   templateUrl: './course-schedule-entry.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./course-schedule-entry.component.css']
 })
 export class CourseScheduleEntryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) {}
 
   ngOnInit() {
+  }
+  closeResult: string;
+
+
+  openBackDropCustomClass(content) {
+    this.modalService.open(content, {backdropClass: 'light-blue-backdrop'});
+  }
+
+  openWindowCustomClass(content) {
+    this.modalService.open(content, { windowClass: 'dark-modal' });
+  }
+
+  openSm(content) {
+    this.modalService.open(content, { size: 'sm' });
+  }
+
+  openLg(content) {
+    this.modalService.open(content, { size: 'lg' });
+  }
+
+  openVerticallyCentered(content) {
+    this.modalService.open(content, { centered: true });
   }
 
 }
