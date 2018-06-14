322516e85fb98a3a8ecf6f1597b6d7871a590e9a
Bob Makowski <tritonpi-git0@yahoo.com>
2018-06-14 17:54:20 -0500

    T:T: faculty retrofitted by http service using adapter pattern

--- fansOfCollinGuitarViaAngular/src/main/ts/src/app/features/lists/links/links.component.ts
+++ fansOfCollinGuitarViaAngular/src/main/ts/src/app/features/lists/links/links.component.ts
@@ -1,21 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import {IGuitarLinks} from "./GuitarLinks";
-import {FileAsSourceForJsonService} from "../../../services/file-as-source-for-json/file-as-source-for-json.service";
+import {
+  FileAsSourceForJsonService,
+  GuitarApiObserver, GuitarApiObserverContract, GuitarApiObserverPollingContract
+} from "../../../services/file-as-source-for-json/file-as-source-for-json.service";
+import {HttpClient} from "@angular/common/http";
+
+const setupUri  = "/assets/json/links-controller.json" ;
 
 @Component({
   selector: 'guitar-links',
   templateUrl: './links.component.html',
   styleUrls: ['./links.component.css']
 })
-export class LinksComponent implements OnInit {
+export class LinksComponent implements OnInit , GuitarApiObserverPollingContract {
 
   guitarProgramLinks : IGuitarLinks[] ;
   constructor(private service: FileAsSourceForJsonService) {  }
+  lookupAgent : GuitarApiObserverContract ;
 
   ngOnInit() {
-    let myAny: any  = this.service.getLinksSetUp() ;
-    this.guitarProgramLinks = myAny ;
-    console.log("linksComponent is HERE:> " + myAny );
+    let clientStub  : HttpClient = this.service.getHttpClient() ;
+    let agent : GuitarApiObserverContract  = new GuitarApiObserver( setupUri , clientStub ) ;
+    this.lookupAgent = agent ;
+    let spun:  boolean = agent.spinUp() ;
+    console.log("spinup is HERE:> " + spun );
   }
+
+  isReady() : boolean {
+    let ret : boolean = false ;
+    if( this.lookupAgent.isReady() ) {
+      let candidate : any = this.lookupAgent.getPayload() ;
+      this.guitarProgramLinks = candidate ;
+      ret = true ;
+    }
+    return ret ;
+  }
+
+  // ngOnInit0() {
+  //   let myAny: any  = this.service.getLinksSetUp() ;
+  //   this.guitarProgramLinks = myAny ;
+  //   console.log("linksComponent is HERE:> " + myAny );
+  // }
 
 }
