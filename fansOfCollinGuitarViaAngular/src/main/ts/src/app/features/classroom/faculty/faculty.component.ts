ae08aec1f60c37accea918142ef4838fb445f9f2
Bob Makowski <tritonpi-git0@yahoo.com>
2018-06-14 19:23:55 -0500

    T:T: faculty feature retrofitted by http service using adapter pattern. also extending an abstract baseclass

--- fansOfCollinGuitarViaAngular/src/main/ts/src/app/features/classroom/faculty/faculty.component.ts
+++ fansOfCollinGuitarViaAngular/src/main/ts/src/app/features/classroom/faculty/faculty.component.ts
@@ -1,12 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {IGuitarFaculty} from "./GuitarFaculty";
 import {
-  FileAsSourceForJsonService, GuitarApiObserver, GuitarApiObserverContract,
+  FileAsSourceForJsonService, GuitarApiComponentBaseClass, GuitarApiObserver, GuitarApiObserverContract,
   GuitarApiObserverPollingContract
 } from "../../../services/file-as-source-for-json/file-as-source-for-json.service";
 import {TabStateComponent} from "../../../services/tab-state/tab-state.component";
 import {IncludeTemplateComponent} from "../../../services/include-template/include-template.component";
 import {HttpClient} from "@angular/common/http";
+import {IHeaderConfig} from "../../../layout/header/HeaderConfig";
 
 const setupUri  = "/assets/json/teacherBiography-controller.json" ;
 
@@ -16,33 +17,27 @@ const setupUri  = "/assets/json/teacherBiography-controller.json" ;
   providers: [FileAsSourceForJsonService,TabStateComponent, IncludeTemplateComponent],
   styleUrls: ['./faculty.component.css']
 })
-export class FacultyComponent implements OnInit, GuitarApiObserverPollingContract {
+export class FacultyComponent extends GuitarApiComponentBaseClass<IGuitarFaculty> implements GuitarApiObserverPollingContract {
   guitarDepartmentFaculty: IGuitarFaculty ;
-  lookupAgent : GuitarApiObserverContract ;
 
-  constructor(public teacherTab: TabStateComponent, private service: FileAsSourceForJsonService ) { }
+  constructor(public teacherTab: TabStateComponent, private service: FileAsSourceForJsonService ) { super( setupUri , service.getHttpClient() ) ;}
 
-  ngOnInit() {
-    let clientStub  : HttpClient = this.service.getHttpClient() ;
-    let agent : GuitarApiObserverContract  = new GuitarApiObserver( setupUri , clientStub ) ;
-    this.lookupAgent = agent ;
-    let spun:  boolean = agent.spinUp() ;
-    console.log("spinup is HERE:> " + spun );
-  }
-
-  // ngOnInit0() {
-  //   let myAny: any  = this.service.getFacultySetUp() ;
-  //   this.guitarDepartmentFaculty = myAny ;
-  //   console.log("facultyComponent is HERE:> " + myAny );
-  // }
-
-  isReady() : boolean {
+  isReady(): boolean {
     let ret : boolean = false ;
-    if( this.lookupAgent.isReady() ) {
-      let candidate : any = this.lookupAgent.getPayload() ;
+    if(this.getNetworker().isReady() ) {
+      let candidate : any = this.getNetworker().getPayload() ;
       this.guitarDepartmentFaculty = candidate ;
       ret = true ;
     }
     return ret ;
   }
+
+  // ngOnInit() {
+  //   let clientStub  : HttpClient = this.service.getHttpClient() ;
+  //   let agent : GuitarApiObserverContract  = new GuitarApiObserver( setupUri , clientStub ) ;
+  //   this.lookupAgent = agent ;
+  //   let spun:  boolean = agent.spinUp() ;
+  //   console.log("spinup is HERE:> " + spun );
+  // }
+
 }
