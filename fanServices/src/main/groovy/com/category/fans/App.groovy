a917aba48d25105adfb5274bf18e881f7ce7fa11
Bob Makowski <tritonpi-git0@yahoo.com>
2018-09-18 15:27:17 -0500

    T:T: fanServices and base project using allproject approach

--- fanServices/src/main/groovy/com/category/fans/App.groovy
+++ fanServices/src/main/groovy/com/category/fans/App.groovy
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication(scanBasePackages = ["com.category.fans"])
-class App {
+class App { // TODO : clean up
 
 //    @RequestMapping(value = "/**/{[path:[^\\.]*}")
 //    public String redirect() {
