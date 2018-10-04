e53a7100d9deacec92443eaf17c632bafebf99de
Bob Makowski <tritonpi-git0@yahoo.com>
2018-10-04 11:30:54 -0500

    T:T: CORS for spring data rest

---
+++ fanServices/src/main/groovy/com/category/fans/config/SpringDataRestCustomization.groovy
@@ -0,0 +1,17 @@
+package com.category.fans.config
+
+import com.category.fans.controller.CrossOriginContract
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter
+import org.springframework.stereotype.Component
+
+@Component
+ class SpringDataRestCustomization extends RepositoryRestConfigurerAdapter {
+    private final String origin = CrossOriginContract.CROSS_SITE_ORIGIN
+
+    @Override
+    void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.getCorsRegistry().addMapping("/api/**")
+                .allowedOrigins(origin)
+    }
+}
