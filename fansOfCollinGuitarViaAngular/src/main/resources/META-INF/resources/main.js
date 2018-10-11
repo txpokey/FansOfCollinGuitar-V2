566914e9a8792c9b5136682bb61e8e9bc775da47
Bob Makowski <tritonpi-git0@yahoo.com>
2018-10-10 21:17:54 -0500

    T:T: proforma: updated META-INF/resource directories per ng build. ready for release

--- fansOfCollinGuitarViaAngular/src/main/resources/META-INF/resources/main.js
+++ fansOfCollinGuitarViaAngular/src/main/resources/META-INF/resources/main.js
@@ -3956,7 +3956,7 @@ var __metadata = (undefined && undefined.__metadata) || function (k, v) {
 
 
 
-var setupUri = "/assets/json/news-controller--2018.summer.json";
+var setupUri = "/assets/json/news-controller--2018.fall.json";
 var NewsComponent = /** @class */ (function (_super) {
     __extends(NewsComponent, _super);
     function NewsComponent(tabStates, service) {
