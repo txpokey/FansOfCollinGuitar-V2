4f3325cc569f6dc0ece01d756f8d654276db3f03
Bob Makowski <tritonpi-git0@yahoo.com>
2019-01-20 16:16:45 -0600

    T:T: updates for Spring 2019 events

--- fanServices/src/main/groovy/com/category/fans/service/bootstrap/repository/CollegeEventsBootstrap.groovy
+++ fanServices/src/main/groovy/com/category/fans/service/bootstrap/repository/CollegeEventsBootstrap.groovy
@@ -6,7 +6,15 @@ import com.category.fans.service.bootstrap.BootstrapContract
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
+//CollegeEventsBootstrap
+
 import java.time.LocalDate
+
+import static com.category.fans.service.bootstrap.repository.events.CollegeEventsBootstrap2019Spring.header as
+BOOTSTRAP_HEADER
+
+import static com.category.fans.service.bootstrap.repository.events.CollegeEventsBootstrap2019Spring.events as
+BOOTSTRAP_EVENTS
 
 @Component("collegeEventsBootstrap")
 class CollegeEventsBootstrap implements BootstrapContract {
@@ -25,85 +33,8 @@ class CollegeEventsBootstrap implements BootstrapContract {
         true
     }
 
-    private final static def header = [
-            [ name : "2018 Fall Guitar Events at Collin College" ,
-              date : "Date" ,
-              time : "Time" ,
-              room : "Location/Room" ,
-              event : "Events"
-            ] ,
-    ]
-    private final static def events = [
-            [ name : "Guitar Get Together" ,
-              date : "8/31/2018" ,
-              time : "Friday 1-3pm" ,
-              room : "C104 Spring Creek" ,
-              event : "Guitar Get Together"
-            ] ,
-            [ name : "Guitar Get Together" ,
-              date : "9/14/2018" ,
-              time : "Friday 1-3pm" ,
-              room : "C104 Spring Creek" ,
-              event : "Guitar Get Together"
-            ] ,
-            [ name : "Guitar Get Together" ,
-              date : "10/12/2018" ,
-              time : "Friday 1-3pm" ,
-              room : "C104 Spring Creek" ,
-              event : "Guitar Get Together"
-            ] ,
-            [ name : "Guitar Get Together" ,
-              date : "10/26/2018" ,
-              time : "Friday 1-3pm" ,
-              room : "C104 Spring Creek" ,
-              event : "Guitar Get Together"
-            ] ,
-            [ name : "Guitar Get Together" ,
-              date : "11/9/2018" ,
-              time : "Friday 1-3pm" ,
-              room : "C104 Spring Creek" ,
-              event : "Guitar Get Together"
-            ] ,
-            [ name : "Guitar Get Together" ,
-              date : "11/30/2018" ,
-              time : "Friday 1-3pm" ,
-              room : "C104 Spring Creek" ,
-              event : "Guitar Get Together"
-            ] ,
-            [ name : "Jay Armstrong" ,
-              date : "10/10/2018" ,
-              time : "Wed 12pm" ,
-              room : "B-187 Spring Creek" ,
-              event : "WNR Student Recital: Jay Armstrong"
-            ] ,
-            [ name : "Aaron Ringo, Luthier" ,
-              date : "10/17/2018" ,
-              time : "Wed 12pm" ,
-              room : "B-187 Spring Creek" ,
-              event : "WNR Recital: Aaron Ringo, Luthier"
-            ] ,
-
-            [ name : "Guitar Artist Showcase" ,
-              date : "11/14/2018" ,
-              time : "Wed 7pm " ,
-              room : "The Arts Gallery (A-175)" ,
-              event : "Guitar Artist Showcase"
-            ]
-            ,
-            [ name : "Solo Guitar Concert " ,
-              date : "12/5/2018" ,
-              time : "Wed 7pm " ,
-              room : "The Arts Gallery (A-175)" ,
-              event : "Solo Guitar Concert "
-            ]
-            ,
-            [ name : "Guitar/Piano Ensembles Concert" ,
-              date : "11/27/2018" ,
-              time : "Tue 7pm" ,
-              room : "John Anthony Theatre  " ,
-              event : "Guitar/Piano Ensembles Concert"
-            ]
-    ].sort {a,b-> compareOnDateField(a, b) }
+    private final static def header = BOOTSTRAP_HEADER
+    private final static def events = BOOTSTRAP_EVENTS.sort { a, b-> compareOnDateField(a, b) }
 
     private static compareOnDateField(LinkedHashMap<String, String> a, LinkedHashMap<String, String> b) {
         LocalDate.parse(a.date, formatter).compareTo(LocalDate.parse(b.date, formatter))
---
+++ fanServices/src/main/groovy/com/category/fans/service/bootstrap/repository/events/CollegeEventsBootstrap2019Spring.groovy
@@ -0,0 +1,125 @@
+package com.category.fans.service.bootstrap.repository.events
+
+class CollegeEventsBootstrap2019Spring {
+
+    final static def getHeader() {
+        header
+    }
+
+    final static def getEvents() {
+        events
+    }
+
+    private final static def header = [
+            [ name : "2019 Spring Guitar Events at Collin College" ,
+              date : "Date" ,
+              time : "Time" ,
+              room : "Location/Room" ,
+              event : "Events"
+            ] ,
+    ]
+
+    private final static def events = [
+            [ name : "Guitar Get Together" ,
+              date : "1/25/2019" ,
+              time : "Friday 1-3pm" ,
+              room : "C104 Spring Creek" ,
+              event : "Guitar Get Together"
+            ] ,
+            [ name : "Guitar Get Together" ,
+              date : "2/22/2019" ,
+              time : "Friday 1-3pm" ,
+              room : "C104 Spring Creek" ,
+              event : "Guitar Get Together"
+            ] ,
+            [ name : "Guitar Get Together" ,
+              date : "03/08/2019" ,
+              time : "Friday 1-3pm" ,
+              room : "C104 Spring Creek" ,
+              event : "Guitar Get Together"
+            ] ,
+            [ name : "Guitar Get Together" ,
+              date : "03/22/2019" ,
+              time : "Friday 1-3pm" ,
+              room : "C104 Spring Creek" ,
+              event : "Guitar Get Together"
+            ] ,
+            [ name : "Guitar Get Together" ,
+              date : "4/05/2019" ,
+              time : "Friday 1-3pm" ,
+              room : "C104 Spring Creek" ,
+              event : "Guitar Get Together"
+            ] ,
+            [ name : "Guitar Get Together" ,
+              date : "5/3/2019" ,
+              time : "Friday 1-3pm" ,
+              room : "C104 Spring Creek" ,
+              event : "Guitar Get Together"
+            ] ,
+            [ name : "Dr. Olga Amelkina" ,
+              date : "2/27/2019" ,
+              time : "Wed 12pm" ,
+              room : "B-187 Spring Creek" ,
+              event : "WNR Faculty Recital: Dr. Olga Amelkina"
+            ] ,
+            [ name : "Guitar Artist Showcase" ,
+              date : "4/17/2019" ,
+              time : "Wed 7pm" ,
+              room : "The Arts Gallery (A-175)" ,
+              event : "Guitar Artist Showcase"
+            ] ,
+            [ name : "Solo Guitar Concert " ,
+              date : "5/7/2019" ,
+              time : "Tues 7pm " ,
+              room : "The Arts Gallery (A-175)" ,
+              event : "Solo Guitar Concert"
+            ]
+            ,
+            [ name : "Guitar/Keyboard Ensembles Concert" ,
+              date : "4/30/2019" ,
+              time : "Tue 7pm" ,
+              room : "John Anthony Theatre  " ,
+              event : "Guitar/Piano Ensembles Concert"
+            ]
+            ,
+            [ name : "7th Annual Collin College Guitar Festival Weekend" ,
+              date : "2/8/2019" ,
+              time : "2/8-10 (FRI-SUN)" ,
+              room : "Spring Creek Campus" ,
+              event : "7th Annual Collin College Guitar Festival Weekend"
+            ]
+            ,
+            [ name : "Faculty Concert: Dr. Robby Gibson and Kithara Duo" ,
+              date : "2/8/2019" ,
+              time : "Fri 7pm" ,
+              room : "The Arts Gallery (A-175)" ,
+              event : "Faculty Concert: Dr. Robby Gibson and Kithara Duo"
+            ]
+            ,
+            [ name : "Alum Faculty Concert: Solazur" ,
+              date : "2/9/2019" ,
+              time : "Sat 2pm" ,
+              room : "The Arts Gallery (A-175)" ,
+              event : "Alum Faculty Concert: Solazur"
+            ]            ,
+            [ name : "Student Concert: Collin College Guitar Festival" ,
+              date : "2/10/2019" ,
+              time : "Sun 3pm" ,
+              room : "Living Legends Conference Center" ,
+              event : "Student Concert: Collin College Guitar Festival"
+            ]
+    ]
+}
+
+//
+//[ name : "Guitar Artist Showcase" ,
+//  date : "11/14/2019" ,
+//  time : "Wed 7pm " ,
+//  room : "The Arts Gallery (A-175)" ,
+//  event : "Guitar Artist Showcase"
+//]
+//
+//2/8-10 (FRI-SUN) 7th Annual Collin College Guitar Festival
+//-2/8 Faculty Concert: Dr. Robby Gibson and Kithara Duo, The ARTS Gallery (A-175) at 7pm
+//-2/9 Alum Faculty Recital: Solazur, The ARTS Gallery (A-175) at 2pm
+//-2/10 Student Concert: Living Legends Conference Center at 3pm
